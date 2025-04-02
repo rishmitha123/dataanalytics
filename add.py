92d36666316947aeba65ddea80efe8a7ad10bd9d
Rishmitha Vankadaru <102401622+rishmitha123@users.noreply.github.com>
2025-04-02 15:38:48 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,36 @@
+# add numbers
+num1=int(input("enter num1 :"))
+num2=int(input("enter num2 :"))
+sum=num1+num2
+print(sum)
+# concatinating strings
+num1=input("enter num1 :")
+num2=input("enter num2 :")
+sum=num1+num2
+print(sum)
+# type of variables
+a=10
+b=2.56
+c="sai"
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+# arithmetic operators
+num1=int(input("enter num1 :"))
+num2=int(input("enter num2 :"))
+sum=num1+num2
+print(sum)
+sub=num1-num2
+print(sub)
+mul=num1*num2
+print(mul)
+div=num1/num2
+print(div)
+mod=num1%num2
+print(mod)
+print("hello","world",sep="-")
+print("hello","world",sep="&")
+
+
