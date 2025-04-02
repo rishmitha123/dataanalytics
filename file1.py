99bd150fe7ac172fceb0f4645e5517ab7274a386
Rishmitha Vankadaru <102401622+rishmitha123@users.noreply.github.com>
2025-04-02 15:40:18 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,7 @@
+name=input("enter your name :")
+age=int(input("enter your age :"))
+address=input("enter your address :")
+print("My Details :")
+print("My name is",name)
+print("My age is",age)
+print("My address is",address)
